047253487b83040eb57187027d4df7e38aa2b9f0
S <77819265+pahkasoft@users.noreply.github.com>
2025-10-30 09:17:47 +0200

    chore: made math-lib root package.

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,5 +1,5 @@
 export * from "./math-object";
 export * from "./math-object-history";
 export * from "./math-object-editor";
-export { Nodes } from "./nodes";
-export { expressionFromValue } from "./expression-from-value";
+export * from "./nodes";
+export * from "./expression-from-value";
